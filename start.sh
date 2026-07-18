b8ebfa58908be9e37e937eab4238192b58fc3dd3
PROLOOKA <aflampo818@gmail.com>
2026-07-18 21:13:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+set -e
 
 mkdir -p /app/hls
 
-ffmpeg -re -i "http://qwerty65.xyz:80/47352164/fSe3AuX/112591" \
+exec ffmpeg \
+-i "http://www.r56mail.bingo:80/jpellat/e6khqnE/112591" \
 -c copy \
 -f hls \
 -hls_time 4 \
 -hls_list_size 5 \
 -hls_flags delete_segments \
 /app/hls/index.m3u8
-
-python3 -m http.server 8080 -d /app/hls
